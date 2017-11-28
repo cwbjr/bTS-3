8ac49db205c4286e8c84dc2255e550e44915b43e
Charles Ballew <charleswballewjr@gmail.com>
2017-11-28 11:00:00 -0700

    updated /:id and added data folder with a file

---
+++ data/staff.js
@@ -0,0 +1,63 @@
+module.exports = {
+    "staff": [{
+        id: 1,
+        fullName: "Kyle Coberly",
+        title: "Faculty Director",
+        numberDogs: 0
+    },{
+        id: 2,
+        fullName: "Danny Fritz",
+        title: "Lead Instructor",
+        numberDogs: 0
+    },{
+        id: 3,
+        fullName: "CJ Reynolds",
+        title: "Lead Instructor",
+        numberDogs: 0
+    },{
+        id: 4,
+        fullName: "Brooks Patton",
+        title: "Lead Instructor",
+        numberDogs: 0
+    },{
+        id: 5,
+        fullName: "Roberto Ortega",
+        title: "Lead Instructor",
+        numberDogs: 1
+    },{
+        id: 6,
+        fullName: "Chad Drummond",
+        title: "Instructor",
+        numberDogs: 0
+    },{
+        id: 7,
+        fullName: "Kim Schlesinger",
+        title: "Instructor",
+        numberDogs: 0
+    },{
+        id: 8,
+        fullName: "Peter Ostiguy",
+        title: "Associate Instructor",
+        numberDogs: 1
+    },{
+        id: 9,
+        fullName: "Cass Torske",
+        title: "Resident",
+        numberDogs: 1
+    },{
+        id: 10,
+        fullName: "Matt Winzer",
+        title: "Resident",
+        numberDogs: 2
+    },{
+        id: 11,
+        fullName: "Aaron Goodman",
+        title: "Resident",
+        numberDogs: 0
+    },{
+        id: 12,
+        fullName: "Michelle Bergquist",
+        title: "Resident",
+        numberDogs: 1
+    }]
+};
