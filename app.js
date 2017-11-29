c1129ec0fc8d4c99572d470c12faba666d73a494
Charles Ballew <charleswballewjr@gmail.com>
2017-11-29 12:14:06 -0700

    guessing game

--- app.js
+++ app.js
@@ -15,13 +15,24 @@ app.get("/:id", (req, res) => {
             return res.json(staff.staffers[i]);
         }
     }
-    if(!staff.staffers[i]) {
-        // res.status = 404;
-        const message = res.json({error: {message: "No record found!"} });
-        res.status(404).send(message);
+    // if(!staff.staffers[i]) {
+    //     // res.status = 404;
+    //     const message = res.json({error: {message: "No record found!"} });
+    //     res.status(404).send(message);
         // res.json({error: {message: "No record found!"}
         // });
-    }
+    // }
+});
+
+app.use(function(req, res, next){
+  res.status(404);
+
+  if (!staff.staffers[i]) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  res.type('txt').send('Not found');
 });
 
 app.listen(process.env.PORT || 3000, () => {
